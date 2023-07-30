cbeb6f32f5c5c5215bb9bbbec8d4697844cdab3b
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2023-07-31 03:44:26 +0700

    final

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} width="200" height="200" alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
 
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
+    <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+12345678"
+      email="b@beyonce.com"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
+    <Card
+      name="Jack Bauer"
+      img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7rFQGuC1j4HT-z0y_WD2MCghq8NejK--KtpMiQbSyGQ&s"
+      tel="+987456412"
+      email="jack@bauer.com"
     />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
 
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
+    <Card
+      name="John Doe"
+      img="https://www.beaconjournal.com/gcdn/presto/2021/03/15/NABJ/bed373b6-58db-41d0-8c1c-f5dc3643394d-johndoebarrel.jpg?crop=619,349,x0,y132&width=619&height=349&format=pjpg&auto=webp"
+      tel="+999915151"
+      email="john@doe.com"
     />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
   </div>,
   document.getElementById("root")
 );
